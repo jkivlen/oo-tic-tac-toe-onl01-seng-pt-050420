50b94846713db82b03e8c421adb89221797398db
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-07 22:52:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,15 @@ end
   end
 
 def won?
-  WIN_COMBINATIONS.any?
+  WIN_COMBINATIONS.any? do |combo|
+    if position_taken?(combo(0)) &&
+      @board[combo(0)] == @board[combo(1)] &&
+      @board[combo(1)] == @board[combo(2)] 
+      return combo
   
+    end
+  end
+ end
+ 
 end
 
