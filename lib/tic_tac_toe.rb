1ca19f5ccea2ffa4b107a2ff856b422783f5a279
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-07 22:56:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,14 +69,15 @@ end
   end
 
 def won?
-  WIN_COMBINATIONS.any? do |combo|
-    if position_taken?(combo(0)) &&
-      @board(combo(0)) == @board(combo(1)) &&
-      @board(combo(1)) == @board(combo(2)) 
-      return combo
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
     end
+      false
   end
- end
+end
  
 end
 
